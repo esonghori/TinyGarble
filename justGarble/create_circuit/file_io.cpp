215d664286d5d28c9b038e4168563a0f53664e59
Siam <siam.umar@gmail.com>
2014-10-03 18:31:39 -0500

    code to read verilog file

---
+++ justGarble/create_circuit/file_io.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "parse_netlist.h"
+#include "file_io.h"
+
+using namespace std;
+
+void write_gate_list(GarbledGate* gate_list, int *circuit_size, string filename){
+	string dfilename("dat/"+filename+"_gate_list.dat");
+	fstream gate_file(dfilename.c_str(),ios::out|ios::binary|ios::trunc);
+	if (!gate_file.good()){ 
+		cout << "Error creating dat file" << endl;
+		exit(1); 
+	}
+	
+	for (int i = 0; i < 3; i++){
+		gate_file << circuit_size[i] << "\n";
+	}
+	
+	int no_of_gates = circuit_size[2];
+	
+	for (int i = 0; i < no_of_gates; i++){
+		gate_file << gate_list[i].input[0].is_port << "\n";
+		gate_file << gate_list[i].input[0].index << "\n";
+		gate_file << gate_list[i].input[1].is_port << "\n";
+		gate_file << gate_list[i].input[1].index << "\n";
+		gate_file << gate_list[i].output.is_port << "\n";
+		gate_file << gate_list[i].output.index << "\n";
+		gate_file << gate_list[i].id << "\n";		
+		gate_file << gate_list[i].type << "\n";
+	}
+	gate_file.close();
+	
+	return;	
+}
+
+void read_gate_list(GarbledGate *&gate_list, int *circuit_size, string filename){
+	string dfilename("dat/"+filename+"_gate_list.dat");
+	fstream gate_file(dfilename.c_str(),ios::in|ios::binary);
+	if (!gate_file.good()){ 
+		cout << "Specified dat file not found" << endl;
+		exit(1); 
+	}
+	gate_file.seekg (0, ios::beg);
+	
+	for (int i = 0; i < 3; i++){
+		gate_file >> circuit_size[i];
+	}
+	
+	int no_of_gates = circuit_size[2];
+	gate_list = new GarbledGate[no_of_gates];
+	
+	for (int i = 0; i < no_of_gates; i++){
+		gate_file >> gate_list[i].input[0].is_port;
+		gate_file >> gate_list[i].input[0].index;
+		gate_file >> gate_list[i].input[1].is_port;
+		gate_file >> gate_list[i].input[1].index;
+		gate_file >> gate_list[i].output.is_port;
+		gate_file >> gate_list[i].output.index;
+		gate_file >> gate_list[i].id;		
+		gate_file >> gate_list[i].type;
+	}
+	gate_file.close();
+	
+	return;	
+}
+
+void write_task_schedule(int *task_schedule, int no_task, string filename){
+	string dfilename("dat/"+filename+"_task_list.dat");
+	fstream task_file(dfilename.c_str(),ios::out|ios::binary|ios::trunc);
+	if (!task_file.good()){ 
+		cout << "Error creating dat file" << endl;
+		exit(1); 
+	}
+	
+	for (int i = 0; i < no_task; i++)
+		task_file << task_schedule[i] << "\n";
+
+}
+
+void read_task_schedule(int *task_schedule, int no_task, string filename){
+	string dfilename("dat/"+filename+"_task_list.dat");
+	fstream task_file(dfilename.c_str(),ios::in|ios::binary);
+	if (!task_file.good()){ 
+		cout << "Specified dat file not found" << endl;
+		exit(1); 
+	}
+	
+	for (int i = 0; i < no_task; i++)
+		task_file >> task_schedule[i];
+
+}
+
