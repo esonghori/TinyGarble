1c7d384441ae0482e7d82563fcfa05fdce763367
Ebrahim Songhori <e.songhori@gmail.com>
2014-10-03 19:17:06 -0500

    sequential update and parameters added

--- netlists/sum/sum.v
+++ netlists/sum/sum.v
@@ -3,17 +3,34 @@
 module sum
 #(
 	parameter N=1024
+	parameter CC=1
 )
 ( 
+	clk,
+	rst
 	a,
 	b,
 	c
 );
-	input  [N-1:0] a;
-	input  [N-1:0] b;
-	output [N:0] c;
+	input  [N/CC-1:0] a;
+	input  [N/CC-1:0] b;
+	output [N/CC-1:0] c;
 
-	assign c = a + b;
+	reg [1:0] carry_on; 
 
+	generate
+	if(CC>1)
+		always@(posedge clk or posedge rst)
+		begin
+			if(rst)
+				carry_on <= 0;
+			else
+			begin
+				{carry_on, c} = a + b + carry_on;
+			end
+		end
+	else
+		assign c = a + b;
+	endgenerate
 endmodule
 
