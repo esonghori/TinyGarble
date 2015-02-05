5433d33ad90ba871ab655dd8101e046c67b5b910
Ebrahim Songhori <e.songhori@gmail.com>
2015-02-04 21:31:33 -0600

    replace mips with MLite MIPS

---
+++ netlists/mips/test/benchmarks/hamming.s
@@ -0,0 +1,36 @@
+# memory configuration 
+# 0: num
+# 1: answer
+# 2: A[0]
+# 3: A[1]
+# ...
+# num+1: A[num-1]
+# num+2: B[0]
+# num+3: B[1]
+# num+4: B[2]
+# ...
+# 2*num+1: B[num-1]
+
+#hamming distance between A and B with the length of num
+hamming:
+	lw 		$9,	0($0) 	#load num into $9
+	sll		$9, $9, 2	
+	addi 	$2,	$0, 8 	#$2 := A 	
+	add 	$3,	$2,	$9  #$3 := B = A + num
+
+	addi 	$10, $0, 0	# answer
+	addi 	$9, $9, 8 	# num += 2 to compare with the end of A
+loop:
+	beq 	$2, $9, end # if A == end of A, we are done
+	
+	lw 		$4, 0($2) #load *A
+	lw 		$5, 0($3) #load *B
+	xor 	$6, $4, $5 # $6==0 if A[i]==B[i]
+	beq 	$6, $0, SAME
+	addi 	$10, $10, 1 # answer++
+SAME:
+	addi $2, $2, 4 # A++
+	addi $3, $3, 4 # B++
+	j loop # jump back to the top
+end:
+	sw $10, 4($0) #store answer
--- netlists/mips/test/test.do
+++ netlists/mips/test/test.do
@@ -1,19 +1,2 @@
-add wave sim:/TestBench/clk \
-sim:/TestBench/UUT/PC/pc_out \
-sim:/TestBench/UUT/Data_Memory/addr \
-sim:/TestBench/UUT/Data_Memory/data_in \
-sim:/TestBench/UUT/Data_Memory/MemWrite \
-sim:/TestBench/UUT/Data_Memory/data_out \
-sim:/TestBench/UUT/Data_Memory/data_mem_in \
-sim:/TestBench/UUT/Register_File/Rs_addr \
-sim:/TestBench/UUT/Register_File/Rt_addr \
-sim:/TestBench/UUT/Register_File/Rd_addr \
-sim:/TestBench/UUT/Register_File/Rd_data \
-sim:/TestBench/UUT/Register_File/RegWrite \
-sim:/TestBench/UUT/Register_File/Rs_data \
-sim:/TestBench/UUT/Register_File/Rt_data \
-sim:/TestBench/UUT/ALU/data1_in \
-sim:/TestBench/UUT/ALU/data2_in \
-sim:/TestBench/UUT/ALU/ALUCtrl \
-sim:/TestBench/UUT/ALU/data
+add wave sim:/Test_Bench/clk
 run -all
