3f5c969949f91e06dfa523f570d2b7f093382487
Siam <siam.umar@gmail.com>
2015-03-13 16:05:55 -0500

    add networking

---
+++ justGarble/include/tcpip.h
@@ -0,0 +1,21 @@
+#ifndef TCPIP
+#define TCPIP
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h> 
+#include <netinet/tcp.h> 
+#include "../include/common.h"
+
+#define BASE 8
+
+int server_init(int );
+int client_init(char*, int);
+void send_block(int , block );
+void recv_block(int , block* );
+
+#endif
