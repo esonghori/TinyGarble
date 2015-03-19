205953f6f326340140547a21a5e2a1ed9c3d99fe
Ebrahim Songhori <e.songhori@gmail.com>
2015-03-18 20:10:11 -0500

    update justGarble full pipeline

--- justGarble/nonGC_sum.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-readNetlist/bin/ReadNetlist.out readNetlist/netlists/sum/sum_syn_1024_1024.v readNetlist/netlists/sum/sum_syn_1024_1024.scd 1024
-
-bin/SCDCircuitTest.out readNetlist/netlists/sum/sum_syn_1024_1024.scd
