118a8d1e57ff4e2134de688ed172059065df8a3b
Ebrahim Songhori <e.songhori@gmail.com>
2015-03-23 19:33:39 -0500

    half-and, not tested

--- justGarble/src/eval.c
+++ justGarble/src/eval.c
@@ -28,32 +28,32 @@
 extern "C" {
 #endif
 
-long evaluateHG(GarbledCircuit *garbledCircuit, block* inputLables, block* initialDFFLable, block *outputs, int sockfd)
+long evaluate(GarbledCircuit *garbledCircuit, block* inputLables, block* initialDFFLable, block *outputs, int sockfd)
 {
+	int n = garbledCircuit->n;
+	int m = garbledCircuit->m;
+	int p = garbledCircuit->p;
+	int c = garbledCircuit->c;
 	long i, j, cid;
-
 	AES_KEY AES_Key;
+
 
 	long startTime = RDTSC;
 
 	AES_set_encrypt_key((unsigned char *) &(garbledCircuit->globalKey), 128, &AES_Key);
 
-
 	for(cid=0;cid<garbledCircuit->c;cid++) //for each clock cycle
 	{
-
 		for(i=0;i<garbledCircuit->n;i++) //inputs
 		{
 			garbledCircuit->wires[i].label0 = inputLables[cid*garbledCircuit->n + i];
 		}
-
 		if(cid == 0) //dff initial value
 		{
 			for(i=0;i<garbledCircuit->p;i++)
 			{
 				garbledCircuit->wires[garbledCircuit->n + i].label0 = initialDFFLable[i];
 			}
-
 		}
 		else //copy latched labels
 		{
@@ -71,7 +71,6 @@ long evaluateHG(GarbledCircuit *garbledCircuit, block* inputLables, block* initi
 			input0 = garbledGate->input0;
 			input1 = garbledGate->input1;
 			output = garbledGate->output;
-
 
 			printf("(%ld, %ld)\n", cid, i);
 			print__m128i(garbledCircuit->wires[input0].label0);
@@ -124,7 +123,7 @@ long evaluateHG(GarbledCircuit *garbledCircuit, block* inputLables, block* initi
 
 				mask[0] = keys[0];
 				mask[1] = keys[1];
-				AES_ecb_decrypt_blks(keys, 2, &(dkCipherContext.K));
+				AES_ecb_encrypt_blks(keys, 2, &(AES_Key));
 
 				for (j = 0; j < 2; j++)
 				{
@@ -132,16 +131,14 @@ long evaluateHG(GarbledCircuit *garbledCircuit, block* inputLables, block* initi
 					print__m128i(keys[j]);
 				}
 
-
 				mask[0] = xorBlocks(mask[0], keys[0]);
-				mask[1] = xorBlocks(mask[0], keys[1]);
+				mask[1] = xorBlocks(mask[1], keys[1]);
 
 				for (j = 0; j < 2; j++)
 				{
 					printf("m(%ld)\t", j);
 					print__m128i(mask[j]);
 				}
-
 
 				G = mask[0];
 				if(sa)
@@ -154,12 +151,10 @@ long evaluateHG(GarbledCircuit *garbledCircuit, block* inputLables, block* initi
 					E = xorBlocks(E, A);
 				}
 
-
 				printf("G\t");
 				print__m128i(G);
 				printf("E\t");
 				print__m128i(E);
-
 
 				C = xorBlocks(E,G);
 
@@ -176,127 +171,6 @@ long evaluateHG(GarbledCircuit *garbledCircuit, block* inputLables, block* initi
 		{
 			outputs[cid*garbledCircuit->m + i] = garbledCircuit->wires[garbledCircuit->outputs[i]].label0;
 
-			printf ("(%ld, %ld)", cid, i);
-			print__m128i(outputs[cid*garbledCircuit->m + i]);
-		}
-	}
-	return (RDTSC - startTime);
-}
-
-long evaluate(GarbledCircuit *garbledCircuit, block* inputLables, block* initialDFFLable, block *outputs, int sockfd)
-{
-	GarbledGate *garbledGate;
-	AES_KEY AES_Key;
-	const __m128i *sched = ((__m128i *) (AES_Key.rd_key));
-	block val;
-
-	block A, B;
-	block *plainText, *cipherText;
-	block zer = zero_block();
-	block tweak;
-	GarbledTable garbledTable;
-	long a, b, i, j, cid, rnds = 10;
-
-
-	long startTime = RDTSC;
-
-
-	AES_set_encrypt_key((unsigned char *) &(garbledCircuit->globalKey), 128, &AES_Key);
-
-
-	for(cid=0;cid<garbledCircuit->c;cid++) //for each clock cycle
-	{
-
-		for(i=0;i<garbledCircuit->n;i++) //inputs
-		{
-			garbledCircuit->wires[i].label0 = inputLables[cid*garbledCircuit->n + i];
-		}
-
-		if(cid == 0) //dff initial value
-		{
-			for(i=0;i<garbledCircuit->p;i++)
-			{
-				garbledCircuit->wires[garbledCircuit->n + i].label0 = initialDFFLable[i];
-			}
-
-		}
-		else //copy latched labels
-		{
-			for(i=0;i<garbledCircuit->p;i++)
-			{
-				int wireIndex = garbledCircuit->D[i];
-				garbledCircuit->wires[garbledCircuit->n + i].label0 = garbledCircuit->wires[wireIndex].label0;
-			}
-		}
-
-		for (i = 0; i < garbledCircuit->q; i++) // for each gates
-		{
-			int input0, input1, output;
-			garbledGate = &(garbledCircuit->garbledGates[i]);
-			input0 = garbledGate->input0;
-			input1 = garbledGate->input1;
-			output = garbledGate->output;
-
-			printf("(%ld, %ld)\n", cid, i);
-			print__m128i(garbledCircuit->wires[input0].label0);
-			print__m128i(garbledCircuit->wires[input1].label0);
-
-			if (garbledGate->type == XORGATE || garbledGate->type == XNORGATE)
-			{
-				garbledCircuit->wires[output].label0 =
-				xorBlocks(garbledCircuit->wires[input0].label0,
-						garbledCircuit->wires[input1].label0);
-			}
-			else if (garbledGate->type == NOTGATE)
-			{
-				garbledCircuit->wires[output].label0 = garbledCircuit->wires[input0].label0;
-			}
-			else
-			{
-				A = DOUBLE(garbledCircuit->wires[input0].label0);
-				B = DOUBLE(DOUBLE(garbledCircuit->wires[input1].label0));
-
-				plainText = &garbledCircuit->wires[output].label0;
-
-				a = getLSB(garbledCircuit->wires[input0].label0);
-				b = getLSB(garbledCircuit->wires[input1].label0);
-				block temp;
-
-				val = xorBlocks(A, B);
-				tweak = makeBlock(cid, i);
-				val = xorBlocks(val, tweak);
-
-				for (j = 0; j < 3; j++)
-				{
-					recv_block(sockfd, &(garbledTable.table[j]));
-
-					printf("t(%ld)\t", j);
-					print__m128i(garbledTable.table[j]);
-				}
-
-				if (a+b==0)
-					cipherText = &zer;
-				else
-					cipherText = &(garbledTable.table[2*a+b-1]);
-
-				temp = xorBlocks(val, *cipherText);
-
-				val = _mm_xor_si128(val, sched[0]);
-				for (j = 1; j < rnds; j++)
-					val = _mm_aesenc_si128(val, sched[j]);
-				val = _mm_aesenclast_si128(val, sched[j]);
-
-				*plainText = xorBlocks(val, temp);
-			}
-			print__m128i(garbledCircuit->wires[output].label0);
-		}
-		
-		printf ("\noutput: \n");
-
-		for (i = 0; i < garbledCircuit->m; i++)
-		{
-			outputs[cid*garbledCircuit->m + i] = garbledCircuit->wires[garbledCircuit->outputs[i]].label0;
-			
 			printf ("(%ld, %ld)", cid, i);
 			print__m128i(outputs[cid*garbledCircuit->m + i]);
 		}
