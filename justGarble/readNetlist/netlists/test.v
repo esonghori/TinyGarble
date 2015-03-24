5be0e28013f5a7211ad53f56f3c5e92812f0f8f5
Ebrahim Songhori <e.songhori@gmail.com>
2015-03-23 19:33:39 -0500

    half-and, not tested

--- justGarble/readNetlist/netlists/test.v
+++ justGarble/readNetlist/netlists/test.v
@@ -1,20 +1,20 @@
 module sum ( clk, rst, g, e, o );
-input  [3:0] g;
-input  [3:0] e;
-output [3:0] o;
+input  g;
+input  e;
+output o;
 input clk, rst;
 
-wire w1, w2, w3;
+wire w1, w2, w3, CO, CI;
 
 
-AND 	U1 ( .A(g[0]), .B(e[0]) .Z(o[0]));
-ANDN 	U2 ( .A(g[1]), .B(e[1]) .Z(o[1]));
-NAND 	U3 ( .A(g[2]), .B(e[2]) .Z(o[2]));
-OR 		U4 ( .A(g[3]), .B(e[3]) .Z(w1) 	);
-ORN 	U5 ( .A(w1),   .B(w2),  .Z(w3) 	);
-NOR 	U6 ( .A(w3),   .B(e[3]),.Z(o[3]));
+XOR U1 (.A(g),  .B(CI), .Z(w1));
+XOR U2 (.A(e),  .B(CI), .Z(w2));
+AND	U3 (.A(w1), .B(w2), .Z(w3));
+XOR U4 (.A(w1), .B(e),  .Z(o) );
+XOR U4 (.A(w3), .B(CI), .Z(CO));
 
-DFF U7 ( .D(w3), .CLK(clk), .RST(rst), .I(1'b0), .Q(w2));
+
+DFF U7 ( .D(CO), .CLK(clk), .RST(rst), .I(1'b0), .Q(CI));
 
 
 endmodule
