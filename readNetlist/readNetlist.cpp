c7258018ca3a2f534339b81e822174c97cd9bbcb
Ebrahim Songhori <e.songhori@gmail.com>
2015-08-15 21:36:04 -0700

    Reformat
    Reformat with Google Code Style

--- readNetlist/readNetlist.cpp
+++ readNetlist/readNetlist.cpp
@@ -19,95 +19,95 @@
 
 void verilog2SCD(const string &infilename, const string &outfilename, int c) {
 
-	ReadCircuitString readCircuitString;
-	ReadCircuit readCircuit;
+  ReadCircuitString readCircuitString;
+  ReadCircuit readCircuit;
 
-	parse_netlist(infilename, readCircuitString);
-	id_assignment(readCircuitString, readCircuit);
-	topological_sort(readCircuit);
+  parse_netlist(infilename, readCircuitString);
+  id_assignment(readCircuitString, readCircuit);
+  topological_sort(readCircuit);
 
-	writeSCD(readCircuit, c, outfilename);
+  writeSCD(readCircuit, c, outfilename);
 }
 
 void writeSCD(const ReadCircuit &readCircuit, int _c, const string &fileName) {
-	ofstream f(fileName.c_str(), ios::out);
-	if (!f.is_open()) {
-		cout << "can't open " << fileName << endl;
-		return;
-	}
+  ofstream f(fileName.c_str(), ios::out);
+  if (!f.is_open()) {
+    cout << "can't open " << fileName << endl;
+    return;
+  }
 
-	int n = readCircuit.no_of_inports; // # of inputs
-	int g = readCircuit.no_of_g_inports; // # of g inputs
-	int p = readCircuit.no_of_dffs; // # of DFF
-	int m = readCircuit.no_of_outports; // # of outputs
-	int q = readCircuit.no_of_gates; // # of gates
-	int c = _c; // # of sequential cycle
+  int n = readCircuit.no_of_inports;  // # of inputs
+  int g = readCircuit.no_of_g_inports;  // # of g inputs
+  int p = readCircuit.no_of_dffs;  // # of DFF
+  int m = readCircuit.no_of_outports;  // # of outputs
+  int q = readCircuit.no_of_gates;  // # of gates
+  int c = _c;  // # of sequential cycle
 
-	f << n << " " << g << " " << p << " " << m << " " << q << " " << c << endl;
+  f << n << " " << g << " " << p << " " << m << " " << q << " " << c << endl;
 
-	/*
-	 * 1st input of each gate
-	 */
-	for (int i = 0; i < q; i++) {
-		int gindex = readCircuit.task_schedule[i];
-		f << readCircuit.gate_list[gindex].input[0] << " ";
-	}
-	f << endl;
-	/*
-	 * 2nd input of each gate
-	 */
-	for (int i = 0; i < q; i++) {
-		int gindex = readCircuit.task_schedule[i];
-		f << readCircuit.gate_list[gindex].input[1] << " ";
-	}
-	f << endl;
-	/*
-	 *type of each gate
-	 */
-	for (int i = 0; i < q; i++) {
-		int gindex = readCircuit.task_schedule[i];
-		f << (int) readCircuit.gate_list[gindex].type << " ";
-	}
-	f << endl;
-	/*
-	 * outputs : output wire index
-	 */
-	for (int i = 0; i < m; i++) {
-		f << readCircuit.output_list[i] << " ";
-	}
-	f << endl;
-	/*
-	 * D : latch index
-	 * it stores a wire index for DFF:
-	 */
-	for (int i = 0; i < p; i++) {
-		f << readCircuit.dff_list[i].input[0] << " "; //D
-	}
-	f << endl;
-	/*
-	 * I : initial value index
-	 * it store the input index or constant value for each DFF.
-	 * I[i] == CONST_ZERO (== -2) : it means the initial value of the DFF is zero. At the first cycle, Garbler should send token[0] to Evaluator.
-	 * I[i] == CONST_ONE (== -3) : it means the initial value of the DFF is one. At the first cycle, Garbler should send token[1] to Evaluator.
-	 * I[i] > 0 : it means the initial value of the DFF is value of actual circuit input (wire I[i]). At the first cycle, Evaluator should received the tokens from the Garbler (using OT or directly).
-	 */
-	//TODO: some of the input wires are only connected to initial signal. Those wires do not require communication in cycles c > 1.
-	for (int i = 0; i < p; i++) {
-		f << readCircuit.dff_list[i].input[1] << " "; //I
-	}
-	f << endl;
+  /*
+   * 1st input of each gate
+   */
+  for (int i = 0; i < q; i++) {
+    int gindex = readCircuit.task_schedule[i];
+    f << readCircuit.gate_list[gindex].input[0] << " ";
+  }
+  f << endl;
+  /*
+   * 2nd input of each gate
+   */
+  for (int i = 0; i < q; i++) {
+    int gindex = readCircuit.task_schedule[i];
+    f << readCircuit.gate_list[gindex].input[1] << " ";
+  }
+  f << endl;
+  /*
+   *type of each gate
+   */
+  for (int i = 0; i < q; i++) {
+    int gindex = readCircuit.task_schedule[i];
+    f << (int) readCircuit.gate_list[gindex].type << " ";
+  }
+  f << endl;
+  /*
+   * outputs : output wire index
+   */
+  for (int i = 0; i < m; i++) {
+    f << readCircuit.output_list[i] << " ";
+  }
+  f << endl;
+  /*
+   * D : latch index
+   * it stores a wire index for DFF:
+   */
+  for (int i = 0; i < p; i++) {
+    f << readCircuit.dff_list[i].input[0] << " ";  //D
+  }
+  f << endl;
+  /*
+   * I : initial value index
+   * it store the input index or constant value for each DFF.
+   * I[i] == CONST_ZERO (== -2) : it means the initial value of the DFF is zero. At the first cycle, Garbler should send token[0] to Evaluator.
+   * I[i] == CONST_ONE (== -3) : it means the initial value of the DFF is one. At the first cycle, Garbler should send token[1] to Evaluator.
+   * I[i] > 0 : it means the initial value of the DFF is value of actual circuit input (wire I[i]). At the first cycle, Evaluator should received the tokens from the Garbler (using OT or directly).
+   */
+  //TODO: some of the input wires are only connected to initial signal. Those wires do not require communication in cycles c > 1.
+  for (int i = 0; i < p; i++) {
+    f << readCircuit.dff_list[i].input[1] << " ";  //I
+  }
+  f << endl;
 
-	f.close();
+  f.close();
 }
 
 int main(int argc, char** argv) {
-	if (argc < 4) {
-		cout << "usage: " << argv[0]
-				<< " <verilog infilename> <scd outfilename> <c>" << endl;
-		return -1;
-	}
-	string infilename(argv[1]);
-	string outfilename(argv[2]);
-	int c = atoi(argv[3]);
-	verilog2SCD(infilename, outfilename, c);
+  if (argc < 4) {
+    cout << "usage: " << argv[0]
+        << " <verilog infilename> <scd outfilename> <c>" << endl;
+    return -1;
+  }
+  string infilename(argv[1]);
+  string outfilename(argv[2]);
+  int c = atoi(argv[3]);
+  verilog2SCD(infilename, outfilename, c);
 }
--- src/garble.cpp
+++ src/garble.cpp
@@ -1,293 +1,274 @@
 /*
  This file is part of JustGarble.
 
-    JustGarble is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ JustGarble is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    JustGarble is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ JustGarble is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with JustGarble.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with JustGarble.  If not, see <http://www.gnu.org/licenses/>.
 
-*/
+ */
 /*
-	This file is part of TinyGarble. It is modified version of JustGarble
-	under GNU license.
+ This file is part of TinyGarble. It is modified version of JustGarble
+ under GNU license.
 
-	TinyGarble is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+ TinyGarble is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-	TinyGarble is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+ TinyGarble is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
-	along with TinyGarble.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU General Public License
+ along with TinyGarble.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "../include/common.h"
 #include "../include/aes.h"
 #include "../include/justGarble.h"
 #include "../include/tcpip.h"
 #include "../include/aes.h"
 
+long garble(GarbledCircuit *garbledCircuit, block* inputLabels,
+            block* initialDFFLabels, block* outputLabels, block* R,
+            int connfd) {
+  AES_KEY AES_Key;
+  long i, j, cid;
 
-long garble(GarbledCircuit *garbledCircuit, block* inputLabels, block* initialDFFLabels, block* outputLabels, block* R, int connfd)
-{
-	AES_KEY AES_Key;
-	long i, j, cid;
+  long startTime = RDTSC;
 
-	long startTime = RDTSC;
+  AES_set_encrypt_key((unsigned char *) &(garbledCircuit->globalKey), 128,
+                      &AES_Key);
 
-	AES_set_encrypt_key((unsigned char *) &(garbledCircuit->globalKey), 128, &AES_Key);
+  for (cid = 0; cid < garbledCircuit->c; cid++)  //for each clock cycle
+      {
 
-	for(cid = 0; cid <garbledCircuit->c; cid++) //for each clock cycle
-	{
+    for (i = 0; i < garbledCircuit->n; i++)  //inputs
+        {
+      garbledCircuit->wires[i].label0 = inputLabels[2
+          * (cid * garbledCircuit->n + i)];
+      garbledCircuit->wires[i].label1 = inputLabels[2
+          * (cid * garbledCircuit->n + i) + 1];
+    }
 
-		for(i=0;i<garbledCircuit->n;i++) //inputs
-		{
-			garbledCircuit->wires[i].label0 = inputLabels[2*(cid*garbledCircuit->n +i)];
-			garbledCircuit->wires[i].label1 = inputLabels[2*(cid*garbledCircuit->n +i) + 1];
-		}
+    if (cid == 0)  //dff initial value
+        {
+      for (i = 0; i < garbledCircuit->p; i++) {
+        garbledCircuit->wires[garbledCircuit->n + i].label0 = initialDFFLabels[2
+            * i];
+        garbledCircuit->wires[garbledCircuit->n + i].label1 = initialDFFLabels[2
+            * i + 1];
+      }
 
-		if(cid == 0) //dff initial value
-		{
-			for(i=0;i<garbledCircuit->p;i++)
-			{
-				garbledCircuit->wires[garbledCircuit->n + i].label0 = initialDFFLabels[2*i];
-				garbledCircuit->wires[garbledCircuit->n + i].label1 = initialDFFLabels[2*i+1];
-			}
+    } else  //copy latched labels
+    {
+      for (i = 0; i < garbledCircuit->p; i++) {
+        int wireIndex = garbledCircuit->D[i];
+        garbledCircuit->wires[garbledCircuit->n + i].label0 = garbledCircuit
+            ->wires[wireIndex].label0;
+        garbledCircuit->wires[garbledCircuit->n + i].label1 = garbledCircuit
+            ->wires[wireIndex].label1;
+      }
+    }
 
-		}
-		else //copy latched labels
-		{
-			for(i=0;i<garbledCircuit->p;i++)
-			{
-				int wireIndex = garbledCircuit->D[i];
-				garbledCircuit->wires[garbledCircuit->n + i].label0 = garbledCircuit->wires[wireIndex].label0;
-				garbledCircuit->wires[garbledCircuit->n + i].label1 = garbledCircuit->wires[wireIndex].label1;
-			}
-		}
+    for (i = 0; i < garbledCircuit->q; i++)  //for each gates
+        {
+      int input0, input1, output;
+      GarbledGate *garbledGate = &(garbledCircuit->garbledGates[i]);
 
-		for(i=0; i< garbledCircuit->q;i++) //for each gates
-		{
-			int input0, input1, output;
-			GarbledGate *garbledGate = &(garbledCircuit->garbledGates[i]);
+      input0 = garbledGate->input0;
+      input1 = garbledGate->input1;
+      output = garbledGate->output;
 
-			input0 = garbledGate->input0;
-			input1 = garbledGate->input1;
-			output = garbledGate->output;
+      printf("(%ld, %ld)\n", cid, i);
+      print__m128i(garbledCircuit->wires[input0].label0);
+      print__m128i(garbledCircuit->wires[input0].label1);
 
-			printf("(%ld, %ld)\n", cid, i);
-			print__m128i(garbledCircuit->wires[input0].label0);
-			print__m128i(garbledCircuit->wires[input0].label1);
+      print__m128i(garbledCircuit->wires[input1].label0);
+      print__m128i(garbledCircuit->wires[input1].label1);
 
-			print__m128i(garbledCircuit->wires[input1].label0);
-			print__m128i(garbledCircuit->wires[input1].label1);
+      if (garbledGate->type == XORGATE) {
+        garbledCircuit->wires[output].label0 = xorBlocks(
+            garbledCircuit->wires[input0].label0,
+            garbledCircuit->wires[input1].label0);
+        garbledCircuit->wires[output].label1 = xorBlocks(
+            garbledCircuit->wires[input0].label1,
+            garbledCircuit->wires[input1].label0);
 
+        print__m128i(garbledCircuit->wires[garbledGate->output].label0);
+        print__m128i(garbledCircuit->wires[garbledGate->output].label1);
 
-			if (garbledGate->type == XORGATE)
-			{
-				garbledCircuit->wires[output].label0 = xorBlocks(garbledCircuit->wires[input0].label0, garbledCircuit->wires[input1].label0);
-				garbledCircuit->wires[output].label1 = xorBlocks(garbledCircuit->wires[input0].label1, garbledCircuit->wires[input1].label0);
+        continue;
+      } else if (garbledGate->type == XNORGATE) {
+        garbledCircuit->wires[output].label0 = xorBlocks(
+            garbledCircuit->wires[input0].label1,
+            garbledCircuit->wires[input1].label0);
+        garbledCircuit->wires[output].label1 = xorBlocks(
+            garbledCircuit->wires[input0].label0,
+            garbledCircuit->wires[input1].label0);
 
+        print__m128i(garbledCircuit->wires[garbledGate->output].label0);
+        print__m128i(garbledCircuit->wires[garbledGate->output].label1);
 
-				print__m128i(garbledCircuit->wires[garbledGate->output].label0);
-				print__m128i(garbledCircuit->wires[garbledGate->output].label1);
+        continue;
+      } else if (garbledGate->type == NOTGATE) {
+        garbledCircuit->wires[output].label0 = garbledCircuit->wires[input0]
+            .label1;
+        garbledCircuit->wires[output].label1 = garbledCircuit->wires[input0]
+            .label0;
 
-				continue;
-			}
-			else if (garbledGate->type == XNORGATE)
-			{
-				garbledCircuit->wires[output].label0 = xorBlocks(garbledCircuit->wires[input0].label1, garbledCircuit->wires[input1].label0);
-				garbledCircuit->wires[output].label1 = xorBlocks(garbledCircuit->wires[input0].label0, garbledCircuit->wires[input1].label0);
+        print__m128i(garbledCircuit->wires[garbledGate->output].label0);
+        print__m128i(garbledCircuit->wires[garbledGate->output].label1);
 
-				print__m128i(garbledCircuit->wires[garbledGate->output].label0);
-				print__m128i(garbledCircuit->wires[garbledGate->output].label1);
+        continue;
+      }
 
-				continue;
-			}
-			else if (garbledGate->type == NOTGATE){
-				garbledCircuit->wires[output].label0 = garbledCircuit->wires[input0].label1;
-				garbledCircuit->wires[output].label1 = garbledCircuit->wires[input0].label0;
+      block A0, A1, B0, B1, C0, C1;
+      block tweak0, tweak1;
+      unsigned short pa, pb;
+      block keys[4];
+      block mask[4];
+      block table[2];
+      block G, E;
 
-				print__m128i(garbledCircuit->wires[garbledGate->output].label0);
-				print__m128i(garbledCircuit->wires[garbledGate->output].label1);
+      unsigned short v = type2V(garbledGate->type);
+      if (v & 1) {
+        A1 = (garbledCircuit->wires[input0].label0);
+        A0 = (garbledCircuit->wires[input0].label1);
+      } else {
+        A0 = (garbledCircuit->wires[input0].label0);
+        A1 = (garbledCircuit->wires[input0].label1);
+      }
 
-				continue;
-			}
+      if (v & 2) {
+        B1 = (garbledCircuit->wires[input1].label0);
+        B0 = (garbledCircuit->wires[input1].label1);
+      } else {
+        B0 = (garbledCircuit->wires[input1].label0);
+        B1 = (garbledCircuit->wires[input1].label1);
+      }
 
+      pa = getLSB(A0);
+      pb = getLSB(B0);
 
-			block A0, A1, B0, B1, C0, C1;
-			block tweak0, tweak1;
-			unsigned short pa,pb;
-			block keys[4];
-			block mask[4];
-			block table[2];
-			block G, E;
+      tweak0 = makeBlock(cid, 2 * i + 0);
+      tweak1 = makeBlock(cid, 2 * i + 1);
 
+      keys[0] = xorBlocks(A0, tweak0);
+      keys[1] = xorBlocks(A1, tweak0);
+      keys[2] = xorBlocks(B0, tweak1);
+      keys[3] = xorBlocks(B1, tweak1);
 
+      for (j = 0; j < 4; j++) {
+        printf("k(%ld)\t", j);
+        print__m128i(keys[j]);
+      }
 
-			unsigned short v = type2V(garbledGate->type);
-			if(v&1)
-			{
-				A1 = (garbledCircuit->wires[input0].label0);
-				A0 = (garbledCircuit->wires[input0].label1);
-			}
-			else
-			{
-				A0 = (garbledCircuit->wires[input0].label0);
-				A1 = (garbledCircuit->wires[input0].label1);
-			}
+      mask[0] = keys[0];
+      mask[1] = keys[1];
+      mask[2] = keys[2];
+      mask[3] = keys[3];
+      AES_ecb_encrypt_blks(keys, 4, &(AES_Key));
 
-			if(v&2)
-			{
-				B1 = (garbledCircuit->wires[input1].label0);
-				B0 = (garbledCircuit->wires[input1].label1);
-			}
-			else
-			{
-				B0 = (garbledCircuit->wires[input1].label0);
-				B1 = (garbledCircuit->wires[input1].label1);
-			}
+      for (j = 0; j < 4; j++) {
+        printf("kd(%ld)\t", j);
+        print__m128i(keys[j]);
+      }
 
-			pa = getLSB(A0);
-			pb = getLSB(B0);
+      mask[0] = xorBlocks(mask[0], keys[0]);
+      mask[1] = xorBlocks(mask[1], keys[1]);
+      mask[2] = xorBlocks(mask[2], keys[2]);
+      mask[3] = xorBlocks(mask[3], keys[3]);
 
-			tweak0 = makeBlock(cid, 2*i+0);
-			tweak1 = makeBlock(cid, 2*i+1);
+      for (j = 0; j < 4; j++) {
+        printf("m(%ld)\t", j);
+        print__m128i(mask[j]);
+      }
 
-			keys[0] = xorBlocks(A0, tweak0);
-			keys[1] = xorBlocks(A1, tweak0);
-			keys[2] = xorBlocks(B0, tweak1);
-			keys[3] = xorBlocks(B1, tweak1);
+      table[0] = xorBlocks(mask[0], mask[1]);
+      if (pb)
+        table[0] = xorBlocks(table[0], *R);
 
-			for (j = 0; j < 4; j++)
-			{
-				printf("k(%ld)\t", j);
-				print__m128i(keys[j]);
-			}
+      G = mask[0];
+      if (pa)
+        G = xorBlocks(G, table[0]);
 
-			mask[0] = keys[0];
-			mask[1] = keys[1];
-			mask[2] = keys[2];
-			mask[3] = keys[3];
-			AES_ecb_encrypt_blks(keys, 4, &(AES_Key));
+      table[1] = xorBlocks(mask[2], mask[3]);
+      table[1] = xorBlocks(table[1], A0);
 
+      E = mask[2];
+      if (pb) {
+        E = xorBlocks(E, table[1]);
+        E = xorBlocks(E, A0);
+      }
 
-			for (j = 0; j < 4; j++)
-			{
-				printf("kd(%ld)\t", j);
-				print__m128i(keys[j]);
-			}
+      printf("G\t");
+      print__m128i(G);
+      printf("E\t");
+      print__m128i(E);
 
-			mask[0] = xorBlocks(mask[0], keys[0]);
-			mask[1] = xorBlocks(mask[1], keys[1]);
-			mask[2] = xorBlocks(mask[2], keys[2]);
-			mask[3] = xorBlocks(mask[3], keys[3]);
+      if (v & 4) {
+        C1 = xorBlocks(G, E);
+        C0 = xorBlocks(*R, C1);
+      } else {
+        C0 = xorBlocks(G, E);
+        C1 = xorBlocks(*R, C0);
+      }
 
-			for (j = 0; j < 4; j++)
-			{
-				printf("m(%ld)\t", j);
-				print__m128i(mask[j]);
-			}
+      garbledCircuit->wires[garbledGate->output].label0 = C0;
+      garbledCircuit->wires[garbledGate->output].label1 = C1;
 
-			table[0] = xorBlocks(mask[0], mask[1]);
-			if(pb)
-				table[0] = xorBlocks(table[0], *R);
+      for (j = 0; j < 2; j++) {
+        printf("t(%ld)\t", j);
+        print__m128i(table[j]);
 
-			G = mask[0];
-			if(pa)
-				G = xorBlocks(G, table[0]);
+        send_block(connfd, table[j]);
+      }
 
-			table[1] = xorBlocks(mask[2], mask[3]);
-			table[1] = xorBlocks(table[1], A0);
+      print__m128i(garbledCircuit->wires[garbledGate->output].label0);
+      print__m128i(garbledCircuit->wires[garbledGate->output].label1);
 
-			E = mask[2];
-			if(pb)
-			{
-				E = xorBlocks(E, table[1]);
-				E = xorBlocks(E, A0);
-			}
+    }
 
+    printf("\noutput: \n");
 
-			printf("G\t");
-			print__m128i(G);
-			printf("E\t");
-			print__m128i(E);
+    for (i = 0; i < garbledCircuit->m; i++) {
+      block o0 = garbledCircuit->wires[garbledCircuit->outputs[i]].label0;
+      block o1 = garbledCircuit->wires[garbledCircuit->outputs[i]].label1;
+      outputLabels[cid * 2 * garbledCircuit->m + 2 * i] = o0;
+      outputLabels[cid * 2 * garbledCircuit->m + 2 * i + 1] = o1;
 
-			if(v&4)
-			{
-				C1 = xorBlocks(G, E);
-				C0 = xorBlocks(*R, C1);
-			}
-			else
-			{
-				C0 = xorBlocks(G, E);
-				C1 = xorBlocks(*R, C0);
-			}
-
-
-			garbledCircuit->wires[garbledGate->output].label0 = C0;
-			garbledCircuit->wires[garbledGate->output].label1 = C1;
-
-
-			for (j = 0; j < 2; j++)
-			{
-				printf("t(%ld)\t", j);
-				print__m128i(table[j]);
-
-				send_block(connfd, table[j]);
-			}
-
-			print__m128i(garbledCircuit->wires[garbledGate->output].label0);
-			print__m128i(garbledCircuit->wires[garbledGate->output].label1);
-
-		}
-
-		printf ("\noutput: \n");
-
-		for(i=0;i<garbledCircuit->m;i++)
-		{
-			block o0 = garbledCircuit->wires[garbledCircuit->outputs[i]].label0;
-			block o1 = garbledCircuit->wires[garbledCircuit->outputs[i]].label1;
-			outputLabels[cid*2*garbledCircuit->m + 2*i] = o0;
-			outputLabels[cid*2*garbledCircuit->m + 2*i+1] = o1;
-
-			printf ("o(%ld,%ld)\n", cid, i);
-			print__m128i(outputLabels[cid*2*garbledCircuit->m + 2*i]);
-			print__m128i(outputLabels[cid*2*garbledCircuit->m + 2*i+1]);
-		}
-	}
-	return (RDTSC - startTime);
+      printf("o(%ld,%ld)\n", cid, i);
+      print__m128i(outputLabels[cid * 2 * garbledCircuit->m + 2 * i]);
+      print__m128i(outputLabels[cid * 2 * garbledCircuit->m + 2 * i + 1]);
+    }
+  }
+  return (RDTSC - startTime);
 }
 
+void createInputLabels(block* inputLabels, block R, int n) {
+  int i;
 
-void createInputLabels(block* inputLabels, block R, int n)
-{
-	int i;
-
-	for (i = 0; i < 2 * n; i += 2)
-	{
-		inputLabels[i] = randomBlock();
-		inputLabels[i + 1] = xorBlocks(R, inputLabels[i]);
-	}
+  for (i = 0; i < 2 * n; i += 2) {
+    inputLabels[i] = randomBlock();
+    inputLabels[i + 1] = xorBlocks(R, inputLabels[i]);
+  }
 }
 
-
-void removeGarbledCircuit(GarbledCircuit *garbledCircuit)
-{
-	free(garbledCircuit->garbledGates);
-	free(garbledCircuit->wires);
-	free(garbledCircuit->outputs);
-	free(garbledCircuit->I);
-	free(garbledCircuit->D);
+void removeGarbledCircuit(GarbledCircuit *garbledCircuit) {
+  free(garbledCircuit->garbledGates);
+  free(garbledCircuit->wires);
+  free(garbledCircuit->outputs);
+  free(garbledCircuit->I);
+  free(garbledCircuit->D);
 }
 
