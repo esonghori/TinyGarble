a3b6e1135905a3fbfa469ee2743a394bca78d4c6
Ebrahim Songhori <e.songhori@gmail.com>
2015-09-22 15:25:33 -0500

    debug multipication circuit

--- genNetlist/mult/mult.v
+++ genNetlist/mult/mult.v
@@ -1,91 +1,44 @@
 `timescale 1ns / 1ps
+// synopsys template
 
-module mult
+module MULT
 #(
-	parameter N=128,
-	parameter CC=1
+	parameter N=8
 )
 ( 
-	clk,
-	rst,
-	a,
-	b,
-	c
+	A,
+	B,
+	O
 );
-	input  clk;
-	input  rst;
-	input  [N-1:0] a;
-	input  [N/CC-1:0] b;
-	output [N-1:0] c;
+	input  [N-1:0] A;
+	input  [N-1:0] B;
+	output [N-1:0] O;
 	
-	reg [2*N-1:0] sreg;
-	wire [N-1:0] swire;
-	wire [N-1:0] clocal;
 
+	wire [N-1:0] w[N-1:0];
 
-	wire  [N-1:0] bexp;
-
-
-	generate
-	if(CC>1)
-	begin
-		assign c = {swire[N/CC-1:0], sreg[N-1:N/CC]};
-		assign bexp = {{(N-N/CC){1'b0}},b};
-		//assign swire  = sreg + {clocal,{(N-N/CC){1'b0}}};
-	end
-	else
-	begin
-		assign c = clocal;
-		assign bexp = b;
-	end		
-	endgenerate
-
-
-	assign clocal = a*bexp;
-	/*MULT 
-	#(
-		.N(N)
-	)
-	MULT_
-	(
-		.A(a),
-		.B(bexp),
-		.O(clocal)
-	);*/	
+	
+	assign w[0] = (A[0])?B:0;
+	assign O = w[N-1];
+	
+	genvar g,h;
 	
 	generate
-	if(CC>1)
-	begin
-		ADD 
-		#(
-			.N(N)
-		)
-		ADD_
-		(
-			.A(sreg[2*N-1:N]),
-			.B(clocal),
-			.CI(1'b0),
-			.S(swire),
-			.CO()
-		);	
+	for(g=1;g<N;g=g+1)
+	begin:FAINST
+			ADD 
+			#(
+				.N(N)
+			) 
+			ADD_
+			(
+				.A(((A[g])?{B[N-1-g:0], {g{1'b0}}}:{N{1'b0}})),
+				.B(w[g-1]), 
+				.CI(1'b0), 
+				.S(w[g]), 
+				.CO()
+			);
 	end
 	endgenerate
-
-	generate
-	if(CC>1)
-		always@(posedge clk or posedge rst)
-		begin
-			if(rst)
-			begin
-				sreg <= 'b0;
-			end
-			else
-			begin
-				sreg <= {{(N/CC){1'b0}}, swire[N-1:0], sreg[N-1:N/CC]};     
-			end
-		end
-	endgenerate
-
-
+	
 endmodule
-
