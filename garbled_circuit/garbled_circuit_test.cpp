c2531657aeed9e8c057acc1d805bee7a776e3848
Ebrahim Songhori <e.songhori@gmail.com>
2015-10-08 01:21:13 -0500

    add plain text test, still not pass for sum 8-bit

--- garbled_circuit/garbled_circuit_test.cpp
+++ garbled_circuit/garbled_circuit_test.cpp
@@ -22,6 +22,7 @@
 #include <openssl/bn.h>
 #include "util/common.h"
 #include "util/log.h"
+#include "util/util.h"
 #include "scd/scd.h"
 
 namespace po = boost::program_options;
@@ -30,6 +31,7 @@ using std::string;
 void EvalauatePlaintext(const GarbledCircuit& garbled_circuit,
                         const BIGNUM* g_init, const BIGNUM* e_init,
                         const BIGNUM* g_input, const BIGNUM* e_input,
+                        uint64_t clock_cycles,
                         BIGNUM** outputs) {
 
   bool* wires = new bool[garbled_circuit.get_wire_size()];
@@ -38,9 +40,9 @@ void EvalauatePlaintext(const GarbledCircuit& garbled_circuit,
     *outputs = BN_new();
   }
   bn_expand(*outputs,
-            garbled_circuit.clock_cycles * garbled_circuit.output_size);
+            clock_cycles * garbled_circuit.output_size);
 
-  for (uint64_t cid = 0; cid < garbled_circuit.clock_cycles; cid++) {  //for each clock cycle
+  for (uint64_t cid = 0; cid < clock_cycles; cid++) {  //for each clock cycle
     //dff initial value
     uint64_t dff_bias = garbled_circuit.get_dff_lo_index();
     if (cid == 0) {
@@ -50,12 +52,12 @@ void EvalauatePlaintext(const GarbledCircuit& garbled_circuit,
           wires[dff_bias + i] = false;
         } else if (wire_index == CONST_ONE) {
           wires[dff_bias + i] = true;
-        } else if (wire_index > 0) {
-          if (wire_index < garbled_circuit.g_init_size) {
+        } else if (wire_index >= 0) {
+          if (wire_index < (int64_t) garbled_circuit.g_init_size) {
             wires[dff_bias + i] = BN_is_bit_set(g_init, wire_index);
           } else {
             wires[dff_bias + i] = BN_is_bit_set(
-                g_init, wire_index - garbled_circuit.g_init_size);
+                g_init, wire_index - (int64_t) garbled_circuit.g_init_size);
           }
         } else {
           LOG(ERROR) << "Invalid I: " << wire_index << endl;
@@ -71,15 +73,17 @@ void EvalauatePlaintext(const GarbledCircuit& garbled_circuit,
 
     // inputs
     uint64_t input_bias = garbled_circuit.get_input_lo_index();
-    for (uint64_t i = 0; i < garbled_circuit.get_input_size(); i++) {
-      if (i < garbled_circuit.g_input_size) {  // input belongs to g
+    for (int64_t i = 0; i < (int64_t) garbled_circuit.get_input_size(); i++) {
+      if (i < (int64_t) garbled_circuit.g_input_size) {  // input belongs to g
         wires[input_bias + i] = BN_is_bit_set(
             g_input, cid * garbled_circuit.g_input_size + i);
+        LOG(INFO) << "g_input " << i << " = " <<  wires[input_bias + i] << endl;
       } else {
         wires[input_bias + i] = BN_is_bit_set(
             e_input,
             cid * garbled_circuit.e_input_size
-                + (i - garbled_circuit.g_input_size));
+                + (i - (int64_t) garbled_circuit.g_input_size));
+        LOG(INFO) << "e_input " << i << " = " <<  wires[input_bias + i] << endl;
       }
     }
 
@@ -110,21 +114,43 @@ int main(int argc, char*argv[]) {
   LogInitial(argc, argv);
 
   string scd_file_address;
-
-  string g_init_str = "";
-  string e_init_str = "";
-  string g_input_str = "";
-  string e_input_str = "";
+  string g_init_str;
+  string e_init_str;
+  string g_input_str;
+  string e_input_str;
+  uint64_t clock_cycles;
   po::options_description desc("");
-  desc.add_options()(
-      "scd_file,i",
-      po::value<string>(&scd_file_address)->default_value(
-          "../read_netlist/netlists/test.scd"),
-      "scd address")  //
-  ("g_init", po::value<string>(&g_init_str), "0")  //
-  ("e_init", po::value<string>(&e_init_str), "0")  //
-  ("g_input", po::value<string>(&g_input_str), "5")  //
-  ("e_input", po::value<string>(&e_input_str), "4");
+  desc.add_options()("help,h", "produce help message")  //
+  ("scd_file,i",
+   po::value<string>(&scd_file_address)->default_value(
+       "../scd/netlists/test4.scd"),
+   "scd address")  // default is sum circuit scd
+  ("clock_cycles", po::value<uint64_t>(&clock_cycles)->default_value(1),
+      "Number of clock cycles to evaluate the circuit.")  //
+   ("g_init", po::value<string>(&g_init_str)->default_value("0"),
+   "g_init in hexadecimal.")  //
+  ("e_init", po::value<string>(&e_init_str)->default_value("0"),
+   "e_init in hexadecimal.")  //
+  ("g_input", po::value<string>(&g_input_str)->default_value("5"),
+   "g_input in hexadecimal.")  //
+  ("e_input", po::value<string>(&e_input_str)->default_value("4"),
+   "e_input in hexadecimal.");
+
+  po::variables_map vm;
+  try {
+    po::parsed_options parsed = po::command_line_parser(argc, argv).options(
+        desc).allow_unregistered().run();
+    po::store(parsed, vm);
+    if (vm.count("help")) {
+      std::cout << desc << endl;
+      return SUCCESS;
+    }
+    po::notify(vm);
+  } catch (po::error& e) {
+    LOG(ERROR) << "ERROR: " << e.what() << endl << endl;
+    std::cout << desc << endl;
+    return FAILURE;
+  }
 
   BIGNUM* g_init = BN_new();
   BIGNUM* e_init = BN_new();
@@ -143,11 +169,11 @@ int main(int argc, char*argv[]) {
   }
 
   BIGNUM* outputs = BN_new();
-  EvalauatePlaintext(garbled_circuit, g_init, e_init, g_input, e_input,
+  EvalauatePlaintext(garbled_circuit, g_init, e_init, g_input, e_input, clock_cycles,
                      &outputs);
 
-  LOG(INFO) << "output:" << endl;
   BN_print_fp(stdout, outputs);
+  std::cout << endl;
 
   LogFinish();
   return SUCCESS;
