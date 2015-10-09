0c081ec19a31242d8f677fdd2cd91c9f790e7c66
Ebrahim Songhori <e.songhori@gmail.com>
2015-10-09 11:08:09 -0500

    add hamming test to scd eval

--- scd/scd_evaluator_test.cpp
+++ scd/scd_evaluator_test.cpp
@@ -31,6 +31,16 @@ void TestSetup() {
 void TestTeardown() {
 }
 
+uint32_t HammingDistance(uint32_t x, uint32_t y) {
+  uint32_t dist = 0;
+  int32_t val = x ^ y;
+  while (val) {
+    dist++;
+    val &= val - 1;
+  }
+  return dist;
+}
+
 MU_TEST(Sum1Bit) {
   string scd_file_address = string(TINYGARBLE_SOURCE_DIR)
       + "/scd/netlists/sum_1bit.scd";
@@ -114,12 +124,72 @@ MU_TEST(Sum8Bit) {
   }
 }
 
+MU_TEST(Hamming32Bit1cc) {
+  string scd_file_address = string(TINYGARBLE_SOURCE_DIR)
+      + "/scd/netlists/hamming_32bit_1cc.scd";
+  int output_mode = 0;  // normal mode
+  string g_init_str = "";
+  string e_init_str = "";
+  uint64_t clock_cycles = 1;
+  for (int i = 0; i < TEST_REPEAT; i++) {
+    uint32_t x[2];
+    x[0] = (uint32_t) rand();
+    x[1] = (uint32_t) rand();
+
+    string g_input_str = to_string_hex(x[0], 8);
+    string e_input_str = to_string_hex(x[1], 8);
+    string output_str = "";
+
+    LOG(INFO) << "Hamming distance between " << g_input_str << " and "
+              << e_input_str << endl;
+
+    EvalauatePlaintextStr(scd_file_address, g_init_str, e_init_str, g_input_str,
+                          e_input_str, clock_cycles, output_mode, &output_str);
+    LOG(INFO) << "result: " << output_str << endl;
+
+    uint32_t y = strtol(output_str.c_str(), nullptr, 16);
+
+    mu_check(y == HammingDistance(x[0], x[1]));
+  }
+}
+
+MU_TEST(Hamming32Bit8cc) {
+  string scd_file_address = string(TINYGARBLE_SOURCE_DIR)
+      + "/scd/netlists/hamming_32bit_8cc.scd";
+  int output_mode = 2;  // print the last cycle
+  string g_init_str = "";
+  string e_init_str = "";
+  uint64_t clock_cycles = 8;
+  for (int i = 0; i < TEST_REPEAT; i++) {
+    uint32_t x[2];
+    x[0] = (uint32_t) rand();
+    x[1] = (uint32_t) rand();
+
+    string g_input_str = to_string_hex(x[0], 8);
+    string e_input_str = to_string_hex(x[1], 8);
+    string output_str = "";
+
+    LOG(INFO) << "Hamming distance between " << g_input_str << " and "
+              << e_input_str << endl;
+
+    EvalauatePlaintextStr(scd_file_address, g_init_str, e_init_str, g_input_str,
+                          e_input_str, clock_cycles, output_mode, &output_str);
+    LOG(INFO) << "result: " << output_str << endl;
+
+    uint32_t y = strtol(output_str.c_str(), nullptr, 16);
+
+    mu_check(y == HammingDistance(x[0], x[1]));
+  }
+}
+
 MU_TEST_SUITE(TestSuite) {
   MU_SUITE_CONFIGURE(&TestSetup, &TestTeardown);
 
   MU_RUN_TEST(Mux8Bit);
   MU_RUN_TEST(Sum1Bit);
   MU_RUN_TEST(Sum8Bit);
+  MU_RUN_TEST(Hamming32Bit1cc);
+  MU_RUN_TEST(Hamming32Bit8cc);
 }
 
 int main(int argc, char *argv[]) {
