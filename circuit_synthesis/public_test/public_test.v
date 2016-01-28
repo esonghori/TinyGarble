4d7bafda10fb3254b1b1642c40286b74fa6cdcc4
Ebrahim Songhori <e.songhori@gmail.com>
2016-01-28 16:44:50 -0600

    complex test

--- circuit_synthesis/public_test/public_test.v
+++ circuit_synthesis/public_test/public_test.v
@@ -1,9 +1,6 @@
 `timescale 1ns / 1ps
 // synopsys_ template
 module public_test
-#(
-  parameter N=8
-)
 ( 
   clk,
   rst,
@@ -18,35 +15,41 @@ module public_test
   
   input clk, rst;
   
-  input  [N-1:0] p_init;
-  input  [N-1:0] g_init;
-  input  [N-1:0] e_init;
+  input  [7:0]  p_init;
+  input  [15:0] g_init;
+  input  [31:0] e_init;
 
-  input  [N-1:0] p_input;
-  input  [N-1:0] g_input;
-  input  [N-1:0] e_input;
-  output [N-1:0] o;
+  input  [31:0] p_input;
+  input  [15:0] g_input;
+  input  [7:0]  e_input;
+  output [31:0] o;
   
 
-  reg [N-1:0] p_reg;
-  reg [N-1:0] g_reg;
-  reg [N-1:0] e_reg;
+  reg [7:0]  reg_1;
+  reg [15:0] reg_2;
+  reg [31:0] reg_3;
   
+  wire [31:0] o1;
+  wire [31:0] o2;
+  wire [31:0] o3;
 
   always@(posedge clk or posedge rst)
   begin
     if(rst) begin
-      p_reg <= p_init;
-      g_reg <= g_init;
-      e_reg <= e_init;
+      reg_1 <= p_init;
+      reg_2 <= g_init;
+      reg_3 <= e_init;
     end else begin
-      p_reg <= p_input ^ p_reg;
-      g_reg <= g_input ^ g_reg;
-      e_reg <= e_input ^ e_reg;
+      reg_1 <= e_input ^ reg_1;
+      reg_2 <= g_input ^ reg_2;
+      reg_3 <= p_input ^ reg_3;
     end
   end
 
-  assign o = ((p_input ^ p_reg)^ (g_input ^ g_reg))^(e_input ^ e_reg);
+  assign o1 = {24'b0, e_input ^ reg_1};
+  assign o2 = {16'b0, g_input ^ reg_2};
+  assign o3 = p_input ^ reg_3;
+  assign o  = o1 ^ o2 ^ o3;
 
 endmodule
 
