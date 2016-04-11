d8a865fec9cbfd820a24c0e1a67158dccaf8ac2c
Ebrahim Songhori <e.songhori@gmail.com>
2016-04-11 17:29:30 -0500

    add a23

---
+++ a23/hamming/hamming.c
@@ -0,0 +1,18 @@
+
+#define G_SIZE 2 // size of Garbler's input array
+#define E_SIZE 2 // size of Evaluator's input array
+#define O_SIZE 1 // size of output array
+
+void gc_main(const int *g,  // Garbler's input array
+    const int *e,           // Evaluator's input array
+    int *o                  // output array
+    ) {
+  int ret = 0;
+  for(int j = 0; j < G_SIZE; j++) {
+    unsigned int t = (unsigned int)(g[j]^e[j]); 
+    for(int i = 0; i < 8*sizeof(int); i++) {
+      ret += (t&(1<<i))>>i;
+    }
+  }
+  o[0] = ret;
+}
