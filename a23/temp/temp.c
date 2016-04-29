f8464e1f66267785500271d9eec92128194bf74b
Ebrahim Songhori <e.songhori@gmail.com>
2016-04-28 21:56:14 -0500

    try compare large

--- a23/temp/temp.c
+++ a23/temp/temp.c
@@ -1,29 +1,18 @@
 
-#define G_SIZE 1 // size of Garbler's input array
-#define E_SIZE 1 // size of Evaluator's input array
+#define G_SIZE 64 // size of Garbler's input array
+#define E_SIZE 64 // size of Evaluator's input array
 #define O_SIZE 1 // size of output array
-
-typedef unsigned char uint8_t;
-typedef unsigned int  uint32_t;
-
-static const uint8_t __attribute__((aligned(64))) sbox[64] =   {
-  //0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
-  0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
-  0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
-  0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
-  0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75};
-
 
 void gc_main(const int *g,  // Garbler's input array
     const int *e,           // Evaluator's input array
     int *o                  // output array
     ) {
 
-  const uint8_t * g8 = (const uint8_t *) g;
-  const uint8_t * e8 = (const uint8_t *) e;
-  uint8_t * o8 =  (uint8_t *) o;
-
-  uint8_t i = g8[0] & 63;
-
-  o8[0] = sbox[i];
+  int less = 0, eq = 1;
+  for (int i = G_SIZE-1; i >= 0; i--)
+  {
+      less = (((g[i] < e[i])^less)&(eq == 1))^(g[i] < e[i]);
+      eq = (((g[i] == e[i])^eq)&(eq == 1))^(g[i] == e[i]);
+  }
+  o[0] = less;
 }
