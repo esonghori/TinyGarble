f11487e6bf131e865b654c43df1c793834f97694
Ebrahim Songhori <e.songhori@gmail.com>
2016-04-29 20:58:58 -0500

    fix hamming dist

--- a23/temp/temp.c
+++ a23/temp/temp.c
@@ -1,18 +1,24 @@
 
-#define G_SIZE 64 // size of Garbler's input array
-#define E_SIZE 64 // size of Evaluator's input array
+#define G_SIZE 32 // size of Garbler's input array
+#define E_SIZE 32 // size of Evaluator's input array
 #define O_SIZE 1 // size of output array
 
-void gc_main(const int *g,  // Garbler's input array
-    const int *e,           // Evaluator's input array
-    int *o                  // output array
+typedef unsigned char uint8_t;
+typedef unsigned int  uint32_t;
+
+inline hasCarry(unsigned int a, unsigned int b, unsigned int s){
+  return (((a>>31)^(s>>31))&((b>>31)^(s>>31)))^(a>>31)^(b>>31)^(s>>31);
+}
+
+
+void gc_main(const unsigned int *g,  // Garbler's input array
+    const unsigned int *e,           // Evaluator's input array
+    unsigned int *o                  // output array
     ) {
 
-  int less = 0, eq = 1;
-  for (int i = G_SIZE-1; i >= 0; i--)
-  {
-      less = (((g[i] < e[i])^less)&(eq == 1))^(g[i] < e[i]);
-      eq = (((g[i] == e[i])^eq)&(eq == 1))^(g[i] == e[i]);
+  int carry = 0;
+  for (int i = 0; i < G_SIZE; i++) {
+    o[i] = g[i] + e[i] + carry;
+    carry = hasCarry(g[i], e[i], o[i]);
   }
-  o[0] = less;
 }
