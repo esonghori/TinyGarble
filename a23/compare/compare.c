e4c449cddaa88e62787ef6e01841abdf5483ec73
Ebrahim Songhori <e.songhori@gmail.com>
2016-08-03 21:39:41 -0700

    fix compare

--- a23/compare/compare.c
+++ a23/compare/compare.c
@@ -1,14 +1,16 @@
-
-#define G_SIZE 1 // size of Garbler's input array
-#define E_SIZE 1 // size of Evaluator's input array
+#define G_SIZE 2 // size of Garbler's input array
+#define E_SIZE 2 // size of Evaluator's input array
 #define O_SIZE 1 // size of output array
 
 void gc_main(const int *g,  // Garbler's input array
-             const int *e,           // Evaluator's input array
-             int *o                  // output array
+             const int *e,  // Evaluator's input array
+             int *o         // output array
             ) {
-  int t = 0;
-  if (g[0] < e[0])
-    t = 1;
-  o[0] = t;
+  int equal = 1;
+  int greater = 0;
+  for(int i = G_SIZE - 1; i >= 0;i--) {
+      greater |= equal & (g[i] < e[i]);
+      equal = equal & (g[i] == e[i]);
+  }
+  o[0] = greater;
 }
