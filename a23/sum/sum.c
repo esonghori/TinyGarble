6a3373f57fe29a41c4a94d74a118b24d6d60a1a7
Ebrahim Songhori <e.songhori@gmail.com>
2016-08-04 23:13:54 -0700

    add sum fix

--- a23/sum/sum.c
+++ a23/sum/sum.c
@@ -4,10 +4,24 @@
 
 typedef unsigned int uint;
 
-void gc_main(const int *g,  // Garbler's input array
-    const int *e,           // Evaluator's input array
-    int *o                  // output array
-    ) {
+// o = g + e
+void sum_array(const int *g, const int *e, int *o);
+
+#ifdef __arm__
+void sum_array(const int *g, const int *e, int *o) {
+  uint carry = 0;
+  for (uint i = 0; i < G_SIZE; ++i) {
+    asm (
+      "cmp %[d], #1\n\t"
+      "adcs %[c], %[a], %[b]\n\t"
+      "mov %[dd], #0\n\t"
+      "movhs %[dd], #1"  :
+      [c] "=r" (o[i]), [dd] "=r" (carry)  : [a] "r" (g[i]), [b] "r" (e[i]), [d] "r" (carry)
+    );
+  }
+}
+#else 
+void sum_array(const int *g, const int *e, int *o) {
   uint carry = 0;
   for (uint i = 0; i < G_SIZE; ++i) {
     o[i] = g[i] + e[i] + carry;
@@ -16,4 +30,13 @@ void gc_main(const int *g,  // Garbler's input array
     uint om = (((uint)o[i])&1<<31)>>31;
     carry = ((gm^om)|(em^om))^om;
   }
+}
+#endif
+
+
+void gc_main(const int *g,  // Garbler's input array
+    const int *e,           // Evaluator's input array
+    int *o                  // output array
+    ) {
+  sum_array(g, e, o);
 }
